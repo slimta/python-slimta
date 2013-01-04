c8fcac8302f2a1873180f7d2721987858e823021
Ian Good <Ian Good ian.good@rackspace.com>
2013-01-04 00:37:37 -0500

    added policy tests

---
+++ test/test_slimta_policy_forward.py
@@ -0,0 +1,46 @@
+
+import unittest
+
+from slimta.policy.forward import Forward
+from slimta.envelope import Envelope
+
+
+class TestPolicyForward(unittest.TestCase):
+
+    def test_no_mappings(self):
+        env = Envelope('sender@example.com', ['rcpt@example.com'])
+        fwd = Forward()
+        fwd.apply(env)
+        self.assertEqual('sender@example.com', env.sender)
+        self.assertEqual(['rcpt@example.com'], env.recipients)
+
+    def test_no_matches(self):
+        env = Envelope('sender@example.com', ['rcpt@example.com'])
+        fwd = Forward()
+        fwd.add_mapping(r'nomatch', 'test')
+        fwd.apply(env)
+        self.assertEqual('sender@example.com', env.sender)
+        self.assertEqual(['rcpt@example.com'], env.recipients)
+
+    def test_simple(self):
+        env = Envelope('sender@example.com', ['rcpt@example.com',
+                                              'test@test.com'])
+        fwd = Forward()
+        fwd.add_mapping(r'^rcpt', 'test')
+        fwd.add_mapping(r'test\.com$', 'example.com')
+        fwd.apply(env)
+        self.assertEqual('sender@example.com', env.sender)
+        self.assertEqual(['test@example.com',
+                          'test@example.com'], env.recipients)
+
+    def test_shortcircuit(self):
+        env = Envelope('sender@example.com', ['rcpt@example.com'])
+        fwd = Forward()
+        fwd.add_mapping(r'^rcpt', 'test')
+        fwd.add_mapping(r'^example', 'testdomain')
+        fwd.apply(env)
+        self.assertEqual('sender@example.com', env.sender)
+        self.assertEqual(['test@example.com'], env.recipients)
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
---
+++ test/test_slimta_policy_headers.py
@@ -0,0 +1,63 @@
+
+import unittest
+import re
+
+from slimta.policy.headers import AddDateHeader, AddMessageIdHeader, \
+                                  AddReceivedHeader
+from slimta.envelope import Envelope
+
+
+class TestPolicyHeaders(unittest.TestCase):
+
+    def test_add_date_header(self):
+        env = Envelope()
+        env.parse('')
+        env.timestamp = 1234567890
+        adh = AddDateHeader()
+        self.assertEqual(None, env.headers['Date'])
+        adh.apply(env)
+        self.assertTrue(env.headers['Date'])
+
+    def test_add_date_header_existing(self):
+        env = Envelope()
+        env.parse('Date: testing\r\n')
+        adh = AddDateHeader()
+        self.assertEqual('testing', env.headers['Date'])
+        adh.apply(env)
+        self.assertEqual('testing', env.headers['Date'])
+
+    def test_add_message_id_header(self):
+        env = Envelope()
+        env.parse('')
+        env.timestamp = 1234567890
+        amih = AddMessageIdHeader('example.com')
+        self.assertEqual(None, env.headers['Message-Id'])
+        amih.apply(env)
+        pattern = r'^<[0-9a-fA-F]{32}\.1234567890@example.com>$'
+        self.assertRegexpMatches(env.headers['Message-Id'], pattern)
+
+    def test_add_message_id_header_existing(self):
+        env = Envelope()
+        env.parse('Message-Id: testing\r\n')
+        amih = AddMessageIdHeader()
+        self.assertEqual('testing', env.headers['Message-Id'])
+        amih.apply(env)
+        self.assertEqual('testing', env.headers['Message-Id'])
+
+    def test_add_received_header(self):
+        env = Envelope('sender@example.com', ['rcpt@example.com'])
+        env.parse('')
+        env.timestamp = 1234567890
+        env.client['name'] = 'mail.example.com'
+        env.client['ip'] = '1.2.3.4'
+        env.client['protocol'] = 'ESMTPS'
+        env.receiver = 'test.com'
+        arh = AddReceivedHeader()
+        arh.apply(env)
+        self.assertRegexpMatches(env.headers['Received'],
+                r'from mail\.example\.com \(unknown \[1.2.3.4\]\) by test.com '
+                r'\(slimta [x0-9]+\.[x0-9]+\.[x0-9]+\) with ESMTPS for '
+                r'<rcpt@example.com>; ')
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
