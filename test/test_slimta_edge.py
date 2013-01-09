e734a2ec0f7d4182a8d23537083b06d4964c3f4f
Ian Good <Ian Good ian.good@rackspace.com>
2013-01-09 14:01:56 -0500

    wrong usage of assertRaises

--- test/test_slimta_edge.py
+++ test/test_slimta_edge.py
@@ -8,7 +8,7 @@ class TestEdge(unittest.TestCase):
 
     def test_edge_interface(self):
         e = Edge(('127.0.0.1', 0), None)
-        self.assertRaises(NotImplementedError, e.handle(None, None))
+        self.assertRaises(NotImplementedError, e.handle, None, None)
 
 
 # vim:et:fdm=marker:sts=4:sw=4:ts=4
--- test/test_slimta_policy.py
+++ test/test_slimta_policy.py
@@ -8,11 +8,11 @@ class TestPolicy(unittest.TestCase):
 
     def test_queuepolicy_interface(self):
         qp = QueuePolicy()
-        self.assertRaises(NotImplementedError, qp.apply(None))
+        self.assertRaises(NotImplementedError, qp.apply, None)
 
     def test_relaypolicy_interface(self):
         rp = RelayPolicy()
-        self.assertRaises(NotImplementedError, rp.apply(None))
+        self.assertRaises(NotImplementedError, rp.apply, None)
 
 
 # vim:et:fdm=marker:sts=4:sw=4:ts=4
