dca3e11af562a6c2ab39ab333d67c2b5d8af5390
Ian Good <Ian Good ian.good@rackspace.com>
2013-01-14 17:17:59 -0500

    apparently this has to be a _

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='python-slimta',
       description='Lightweight, asynchronous SMTP libraries',
       license='MIT',
       url='http://slimta.org/',
-      requires=['gevent', 'gevent-subprocess', 'dnspython'],
+      requires=['gevent', 'gevent_subprocess', 'dnspython'],
       packages=find_packages())
 
 # vim:et:fdm=marker:sts=4:sw=4:ts=4
