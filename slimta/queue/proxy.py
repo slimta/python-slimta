972ffd5e895e83c0c9c40ac3dab88d9e0d634ed6
Ian Good <Ian Good ian.good@rackspace.com>
2013-02-08 16:04:07 -0500

    Queue-like class which proxies to a Relay

---
+++ slimta/queue/proxy.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2012 Ian C. Good
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+"""Package providing an alternative to the standard |Queue| class, such that
+messages are not written to storage but instead immediately relayed. An |Edge|
+service will have to wait for a message to finish relaying before its reply can
+be issued.
+
+"""
+
+import uuid
+
+from slimta.queue import QueueError
+from slimta.relay import RelayError
+
+
+class ProxyQueue(object):
+    """Class implementing the same interface as |Queue|, but proxies a message
+    to a |Relay| service instead of storing and trying/retrying delivery.
+
+    :param relay: |Relay| object used to attempt message deliveries.
+
+    """
+
+    def __init__(self, relay):
+        self.relay = relay
+
+    def start(self):
+        # No-op, because this class does not inherit from Greenlet. Provided
+        # for backwards compatibility with the standard Queue class.
+        pass
+
+    def flush(self):
+        # No-op, because this class does not maintain an actual queue. Provided
+        # for backwards compatibility with the standard Queue class.
+        pass
+
+    def enqueue(self, envelope):
+        try:
+            self.relay._attempt(envelope, 0)
+        except RelayError, e:
+            return [(envelope, e)]
+        else:
+            return [(envelope, uuid.uuid4().hex)]
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
---
+++ test/test_slimta_queue_proxy.py
@@ -0,0 +1,51 @@
+
+import unittest
+
+from mox import MoxTestBase, IsA
+
+from slimta.queue.proxy import ProxyQueue
+from slimta.smtp.reply import Reply
+from slimta.relay import Relay, TransientRelayError, PermanentRelayError
+from slimta.envelope import Envelope
+
+
+class TestProxyQueue(MoxTestBase):
+
+    def setUp(self):
+        super(TestProxyQueue, self).setUp()
+        self.relay = self.mox.CreateMock(Relay)
+        self.env = Envelope('sender@example.com', ['rcpt@example.com'])
+
+    def test_enqueue(self):
+        self.relay._attempt(self.env, 0)
+        self.mox.ReplayAll()
+        q = ProxyQueue(self.relay)
+        ret = q.enqueue(self.env)
+        self.assertEqual(1, len(ret))
+        self.assertEqual(2, len(ret[0]))
+        self.assertEqual(self.env, ret[0][0])
+        self.assertRegexpMatches(ret[0][1], r'[0-9a-fA-F]{32}')
+
+    def test_enqueue_relayerror(self):
+        err = PermanentRelayError('msg failure', Reply('550', 'Not Ok'))
+        self.relay._attempt(self.env, 0).AndRaise(err)
+        self.mox.ReplayAll()
+        q = ProxyQueue(self.relay)
+        ret = q.enqueue(self.env)
+        self.assertEqual(1, len(ret))
+        self.assertEqual(2, len(ret[0]))
+        self.assertEqual(self.env, ret[0][0])
+        self.assertEqual(err, ret[0][1])
+
+    def test_start_noop(self):
+        self.mox.ReplayAll()
+        q = ProxyQueue(self.relay)
+        q.start()
+
+    def test_flush_noop(self):
+        self.mox.ReplayAll()
+        q = ProxyQueue(self.relay)
+        q.flush()
+
+
+# vim:et:fdm=marker:sts=4:sw=4:ts=4
