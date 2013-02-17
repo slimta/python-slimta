ccc82174020a514844eaac3bc2dbe643931b99f3
Ian Good <Ian Good ian.good@rackspace.com>
2013-02-17 13:41:22 -0500

    extracted version file

--- extra/maildrop/slimta/maildroprelay/version.py
+++ extra/maildrop/slimta/maildroprelay/version.py
@@ -19,31 +19,9 @@
 # THE SOFTWARE.
 #
 
-from setuptools import setup, find_packages
+__all__ = ['VERSION']
 
-from slimta.maildroprelay.version import VERSION
-
-
-setup(name='python-slimta-maildrop',
-      version=VERSION,
-      author='Ian Good',
-      author_email='icgood@gmail.com',
-      description='Lightweight, asynchronous SMTP libraries',
-      license='MIT',
-      url='http://slimta.org/',
-      packages=find_packages(),
-      namespace_packages=['slimta'],
-      install_requires=['gevent',
-                        'gevent_subprocess'],
-      tests_require=['nose',
-                     'mox'],
-      test_suite = 'nose.collector',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Topic :: Communications :: Email :: Mail Transport Agents',
-                   'Intended Audience :: Developers',
-                   'Intended Audience :: Information Technology',
-                   'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python'])
+VERSION = '0.1.0'
 
 
 # vim:et:fdm=marker:sts=4:sw=4:ts=4
