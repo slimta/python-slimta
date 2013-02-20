34258e33d203a124750a9f64ad01cf9cd3423af3
Ian Good <ian.good@rackspace.com>
2013-02-19 19:59:57 -0500

    connect slimta docs to celery docs

--- doc/conf.py
+++ doc/conf.py
@@ -251,7 +251,8 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None,
                        'http://gevent.org/': 'http://gevent.org/objects.inv',
-                       'http://redis-py.readthedocs.org/': 'http://redis-py.readthedocs.org/en/latest/objects.inv'}
+                       'http://redis-py.readthedocs.org/': 'http://redis-py.readthedocs.org/en/latest/objects.inv',
+                       'http://docs.celeryproject.org/': 'http://docs.celeryproject.org/en/latest/objects.inv'}
 
 autodoc_member_order = 'bysource'
 
