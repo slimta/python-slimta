f995aa0249c2f4e3037f647f90a411c97b77ec42
Ian Good <Ian Good ian.good@rackspace.com>
2013-04-04 20:17:12 -0400

    upped extra package version numbers

--- extra/diskstorage/setup.py
+++ extra/diskstorage/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-slimta-diskstorage',
-      version='0.1.0',
+      version='0.1.1',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Lightweight, asynchronous SMTP libraries',
