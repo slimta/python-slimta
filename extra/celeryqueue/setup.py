b93d683ca2ee712a5127ffb700de66e6ebab6ab5
Ian Good <ian.good@rackspace.com>
2013-04-29 23:37:36 -0400

    upped celeryqueue version number

--- extra/celeryqueue/setup.py
+++ extra/celeryqueue/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-slimta-celeryqueue',
-      version='0.2.0',
+      version='0.2.1',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Lightweight, asynchronous SMTP libraries',
