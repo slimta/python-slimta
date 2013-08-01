4bacb733817c5ad49e17c80ecb8dfbc9b16ea66d
Ian Good <ian.good@rackspace.com>
2013-08-01 11:10:19 -0400

    better testing on headers

--- test/test_slimta_logging_http.py
+++ test/test_slimta_logging_http.py
@@ -17,13 +17,29 @@ class TestHttpLogger(unittest.TestCase):
 
     @log_capture()
     def test_wsgi_request(self, l):
-        self.log.wsgi_request(self.environ)
-        l.check(('test', 'DEBUG', 'http:{0}:server_request headers=[(\'Header\', \'value\')] method=\'GET\' path=\'/test/stuff\''.format(id(self.environ))))
+        environ1 = {'REQUEST_METHOD': 'GET',
+                    'PATH_INFO': '/test/stuff',
+                    'CONTENT_LENGTH': 'value'}
+        environ2 = {'REQUEST_METHOD': 'GET',
+                    'PATH_INFO': '/test/stuff',
+                    'CONTENT_TYPE': 'value'}
+        environ3 = {'REQUEST_METHOD': 'GET',
+                    'PATH_INFO': '/test/stuff',
+                    'HTTP_X_HEADER_NAME': 'value'}
+        self.log.wsgi_request(environ1)
+        self.log.wsgi_request(environ2)
+        self.log.wsgi_request(environ3)
+        l.check(('test', 'DEBUG', 'http:{0}:server_request headers=[(\'Content-Length\', \'value\')] method=\'GET\' path=\'/test/stuff\''.format(id(environ1))),
+                ('test', 'DEBUG', 'http:{0}:server_request headers=[(\'Content-Type\', \'value\')] method=\'GET\' path=\'/test/stuff\''.format(id(environ2))),
+                ('test', 'DEBUG', 'http:{0}:server_request headers=[(\'X-Header-Name\', \'value\')] method=\'GET\' path=\'/test/stuff\''.format(id(environ3))))
 
     @log_capture()
     def test_wsgi_response(self, l):
-        self.log.wsgi_response(self.environ, '200 OK', [('Header', 'value')])
-        l.check(('test', 'DEBUG', 'http:{0}:server_response headers=[(\'Header\', \'value\')] status=\'200 OK\''.format(id(self.environ))))
+        environ = {'REQUEST_METHOD': 'GET',
+                   'PATH_INFO': '/test/stuff',
+                   'HTTP_HEADER': 'value'}
+        self.log.wsgi_response(environ, '200 OK', [('Header', 'value')])
+        l.check(('test', 'DEBUG', 'http:{0}:server_response headers=[(\'Header\', \'value\')] status=\'200 OK\''.format(id(environ))))
 
     @log_capture()
     def test_request(self, l):
