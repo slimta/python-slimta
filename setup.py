9ab18ce27f28acd5801c2812ad456a4ce8280cd1
Ian Good <icgood@gmail.com>
2016-01-05 15:47:11 +0000

    Set version 3.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='python-slimta',
-      version='3.0.0',
+      version='3.0.1',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Lightweight, asynchronous SMTP libraries.',
